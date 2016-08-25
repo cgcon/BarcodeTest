d01b853373c81cb48a64b516d32771deb297065b
cgcon <cgconde@gmail.com>
2016-08-25 16:41:36 -0300

    Initial commit

---
+++ app/src/main/java/cconde/barcodetest/BarcodeScan.java
@@ -0,0 +1,13 @@
+package cconde.barcodetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BarcodeScan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barcode_scan);
+    }
+}
